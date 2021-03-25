8df3b3c9635fcd4fd3a0e6bd376101237851c339
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:56:12 +0100

    Ninject Binding

---
+++ Marino/Ninject.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security;
+using Marino.DB;
+using Marino.Implementation;
+using Ninject.Modules;
+using TAP2018_19.AuctionSite.Interfaces;
+
+namespace Marino
+{
+    [SecurityCritical]
+    public class AuctionSiteNinjectModule : NinjectModule
+    {
+        [SecurityCritical]
+        public override void Load()
+        {
+            Database.SetInitializer<BaseContext>(null);
+            Bind<ISiteFactory>().To<SiteFactory>();
+            Bind<ISite>().To<Site>();
+            Bind<IAuction>().To<Auction>();
+            Bind<IUser>().To<User>();
+            Bind<ISession>().To<Session>();  
+        }
+    }
+}
