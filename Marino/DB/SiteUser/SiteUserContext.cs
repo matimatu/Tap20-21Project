d2d006bd741c279e4791eae624002a8e71ad23bf
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:45:00 +0100

    SiteUser context
    used in some transactions to not use TotalContext

---
+++ Marino/DB/SiteUser/SiteUserContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Marino.DB
+{
+    internal class SiteUserContext : BaseContext
+    {
+        public SiteUserContext(string connectionString) : base(connectionString)
+        {
+        }
+        public DbSet<SiteEntity> Sites { get; set; }
+        public DbSet<UserEntity> Users{ get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new SiteConf());
+            modelBuilder.Configurations.Add(new UserConf());
+        }
+    }
+}
