0b5cfda36d5aa26c065b7c25d8d554efeafe3295
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:46:14 +0100

    Auction in the EF

---
+++ Marino/DB/Auction/AuctionContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace Marino.DB
+{
+    internal class AuctionContext : BaseContext
+    {
+        public AuctionContext(string connectionString) : base(connectionString)
+        {
+        }
+        public DbSet<AuctionEntity> Auctions { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AuctionConf());
+            modelBuilder.Configurations.Add(new UserConf());
+        }
+    }
+}
