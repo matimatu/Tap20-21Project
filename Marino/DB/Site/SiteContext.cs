e954269267b35778855b0cf4022441ed95b3a178
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:39:55 +0100

    Site in EF

---
+++ Marino/DB/Site/SiteContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Marino.DB
+{
+    internal class SiteContext : BaseContext
+    {
+        public SiteContext(string connectionString) : base(connectionString)
+        {
+        }
+        public DbSet<SiteEntity> Sites { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new SiteConf());
+            modelBuilder.Configurations.Add(new UserConf());
+            modelBuilder.Configurations.Add(new AuctionConf());
+        }
+    }
+}
