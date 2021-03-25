c33f862a02d6ea896bb834d722dd4c5dea8f6254
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:52:46 +0100

    added other contexts and an auxiliary class to hash passwords

---
+++ Marino/DB/OtherContexts/AuxiliaryEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Marino.DB
+{
+    public static class Auxiliary
+    {
+        public const int HashedPwSize = 24;
+        public const int SaltSize = 24;
+        public const int HashedPwOccupation = (HashedPwSize + SaltSize) / 3 * 4;
+        public const int IterationNumber = 100000;
+
+    }
+}
