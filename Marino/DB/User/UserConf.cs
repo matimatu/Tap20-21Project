865b32f8265d40c8058066f9b59d1876ff2814a0
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 11:45:29 +0100

    User in the EF

---
+++ Marino/DB/User/UserConf.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace Marino.DB
+{
+    internal class UserConf: EntityTypeConfiguration<UserEntity>
+    {
+        public UserConf()
+        {
+            ToTable("Users");
+            HasRequired(u => u.Site)
+                .WithMany(s => s.Users)
+                .HasForeignKey(u => u.SiteName);
+                //.WillCascadeOnDelete(false);
+            HasIndex(u => new {u.Name, u.SiteName})
+                .IsUnique(true);
+        }
+    }
+}
---
+++ Marino/DB/User/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Marino.DB
+{
+    internal class UserContext : BaseContext
+    {
+        public UserContext(string connectionString) : base(connectionString)
+        {
+        }
+        public DbSet<UserEntity> Users { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new UserConf());
+            modelBuilder.Configurations.Add(new SessionConf());
+        }
+
+
+    }
+}
