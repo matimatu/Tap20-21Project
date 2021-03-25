0d0acedd6c694327e14cb2ad08eca6954df073b8
matimatu <56006110+matimatu@users.noreply.github.com>
2021-03-25 12:04:24 +0100

    Basic Tests

---
+++ Marino.Tests/BaseClasses.cs
@@ -0,0 +1,57 @@
+﻿namespace Marino.Tests {
+    using System;
+    using TAP2018_19.AlarmClock.Interfaces;
+    using TAP2018_19.AuctionSite.Interfaces;
+    using NUnit.Framework;
+    using Ninject;
+
+    internal static class Configuration {
+        internal const string ImplementationAssembly =
+            @"..\..\..\Marino\bin\Debug\Marino.dll";
+
+        internal const string ConnectionString =
+            @"Data Source=.\SQLEXPRESS;Initial Catalog=AuctionDb;Integrated Security=True;";
+    }
+
+    [TestFixture]
+    public abstract class AbstractTest {
+        protected static readonly IAlarmClockFactory AnAlarmClockFactory;
+        protected static readonly ISiteFactory AnAuctionSiteFactory;
+        protected static readonly string ImplementationAssembly = Configuration.ImplementationAssembly;
+
+        public static ISiteFactory LoadSiteFactoryFromModule() {
+            var kernel = new StandardKernel();
+            ISiteFactory result = null;
+            try {
+                kernel.Load(Configuration.ImplementationAssembly);
+                result = kernel.Get<ISiteFactory>();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+
+            return result;
+        }
+
+        static AbstractTest() {
+            var kernel = new StandardKernel();
+
+            try {
+                AnAuctionSiteFactory = LoadSiteFactoryFromModule();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    public abstract class AuctionSiteTest : AbstractTest {
+        protected ISiteFactory GetSiteFactory() {
+            return AbstractTest.AnAuctionSiteFactory;
+        }
+
+        protected string GetConnectionString() {
+            return Configuration.ConnectionString;
+        }
+    }
+}
